4a81821a1f5454a4bd8de23660b568bafd74cd97
Fucke3r <96577962+Fucke3r@users.noreply.github.com>
2024-08-08 21:35:28 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["💻 Killua Development","🔥 High quality scripts/ui"];
 
 
 let currentIndex = 0;
